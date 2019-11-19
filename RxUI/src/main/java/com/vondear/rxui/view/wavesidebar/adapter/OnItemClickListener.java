6a3c2be2b6eda4cc94ec80111188ad7e14341d42
魏元生 <Alone>
2019-11-19 15:46:34 +0800

    提交

---
+++ RxUI/src/main/java/com/vondear/rxui/view/wavesidebar/adapter/OnItemClickListener.java
@@ -0,0 +1,7 @@
+package com.vondear.rxui.view.wavesidebar.adapter;
+
+
+public interface OnItemClickListener {
+
+    void onItemClick(BaseViewHolder vh, int position);
+}
---
+++ RxUI/src/main/java/com/vondear/rxui/view/wavesidebar/adapter/OnItemLongClickListener.java
@@ -0,0 +1,6 @@
+package com.vondear.rxui.view.wavesidebar.adapter;
+
+
+public interface OnItemLongClickListener {
+    void onItemLongClick(BaseViewHolder vh, int position);
+}
